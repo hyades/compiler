084a25d692bda516a71e058983d7d78dd5093336
Mayank Gupta <genee26@gmail.com>
2013-03-06 19:06:58 +0530

    lexer header files defined
    Symbols need to be done.

--- lexer.h
+++ lexer.h
@@ -1,6 +1,11 @@
 #ifndef LEXER_H_INCLUDED
 #define LEXER_H_INCLUDED
-
-
-
+FILE *getStream(FILE *fp, buffer B, buffersize k);
+tokenInfo getNextToken(FILE *fp);
+char getNextChar(FILE *fp, bool *back);
+token keywordId(char *lexeme, keywordTable kt);//returns token for lexeme if it is a keyword or fieldname
+token mainFun(char *lexeme, keywordTable kt);//returns token for lexeme if it is main or function
+void addKeyword(keywordTable kt, char *keyword, symbol s);
+int hash(char *keyword);//find hash value of string based on key K
+void initkt(keywordTable kt);
 #endif // LEXER_H_INCLUDED
--- lexerDef.h
+++ lexerDef.h
@@ -1,6 +1,26 @@
 #ifndef LEXERDEF_H_INCLUDED
 #define LEXERDEF_H_INCLUDED
+typedef char[] *buffer;
+typdef int buffersize;
+buffersize k=1024;
 
+typedef enum {/*tokens come here*/} symbol;
+
+typedef enum{FALSE,TRUE} bool;
+
+typedef struct{
+	symbol s;
+	char lexeme[100];
+}token;
+typedef token* ptoken;
+
+typedef struct{
+	bool present;
+	symbol s;
+	char keyword[100];
+}keyword;
+
+typedef keyword* keywordTable;
 
 
 #endif // LEXERDEF_H_INCLUDED
