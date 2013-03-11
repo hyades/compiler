012cd5e9377462b02bcc608fd41cdb441bdba2e3
hyades <aayushahuja@gmail.com>
2013-03-11 22:17:32 +0530

    almost parser table

--- run2.c
+++ run2.c
@@ -23,35 +23,33 @@ int main(int argc, char *argv[])
 {
     FILE *s=fopen("set.txt", "r");
     FILE *g=fopen("rules.txt", "r");
-    FILE *fp=fopen("parsetable.txt", "w");
+    FILE *fp=fopen("parsetable.csv", "w");
     Table T[60][60];
     grammar G[70];
     sets S[60];
 
-    keyword kn[200];
+    keyword kn[2000];
     int i,j,Gno,Sno;
-    for(i=0; i<200; i++)
+    for(i=0; i<2000; i++)
         kn[i].present=FALSE;
     keywordTable nt = kn;
     initNt(nt);
     Gno = createGrammar(g,G,nt);
     
     Sno = createSets(s,S,nt);
-    
-    for(i=0;i<Sno;i++)
+/*    for(i=0;i<Sno;i++)
     {
-    	printf("\n%d %d %s %d\n", S[i].firstno,S[i].followno, toStr(G[i].nt), S[i].eps);
+    	printf("\n%d %d %s %d\n", S[i].firstno,S[i].followno, toStr(S[i].nt), S[i].eps);
     	for(j=0;j<S[i].firstno;j++)
     		printf("%s ",toStr(S[i].first[j] ));
     	printf("\n");
     	for(j=0;j<S[i].followno;j++)
     		printf("%s ",toStr(S[i].follow[j] ));
     }
-    /*
-    initTable(T);
+*/    initTable(T);
     createParseTable(G,T,S,Gno);
     printTable(fp, T);
 	fclose(fp);
-	*/
+	
     return 0;
 }
