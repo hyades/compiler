c53e28782374799ce511a27daf03472472175dcb
hyades <aayushahuja@gmail.com>
2013-04-17 22:33:36 +0530

    merged agin

--- semantic.c
+++ semantic.c
@@ -25,15 +25,29 @@ semantic.c
 
 int checkMultift(funTable FT[],char* fname,int recindex,char* name)
 {
-
+	int hval,hval2,hkey=100;
+	hval = hash(fname,hkey);
+	hval2 = hash(name,hkey);
+	if(FT[hval].table[hval2].filled)
+		printf("ERROR: Variable %s has been declared multiple times in function %s\n",name,fname);
+	return FT[hval].table[hval2].filled;
 }
 
 int checkMultirt(recTable RT[],char* rname,char* name)
 {
-
+	int hval,hval2,hkey=100;
+	hval = hash(rname,hkey);
+	hval2 = hash(name,hkey);
+	if(RT[hval].table[hval2].filled)
+		printf("ERROR: Variable %s has been declared multiple times in record %s\n",name,rname);
+	return RT[hval].table[hval2].filled;
 }
 
 int checkMultigt(variable GT[],int recindex,char* name)
 {
-	
+	int hval,hkey=100;
+	hval = hash(name,hkey);
+	if(GT[hval].filled)
+		printf("ERROR: Variable %s has been declared multiple times globally\n",name);
+	return GT[hval].filled;
 }
